a448238108eab3d9982ca0afec8022c222c96a11
Satyam Jaiswal <jaissat92@gmail.com>
2022-05-18 01:29:14 +0530

    modified AddOrg Form

--- src/Components/Organizations/AddOrgForm/AddOrgForm.jsx
+++ src/Components/Organizations/AddOrgForm/AddOrgForm.jsx
@@ -4,32 +4,14 @@ import styles from './AddOrgForm.module.css'
 
 export default function AddOrgForm() {
 
-    function populateOrgList() {
-        return (demoOrgList.map((org) => <option value={org.orgDomain}>{org.orgTitle}</option>));
-    }
-
-    const [selectDomain, setSelectDomain] = useState("");
-    function updateDomain(event) {
-        setSelectDomain(event.target.value);
-    }
-
     function handleSubmit(event) {
         event.preventDefault();
-        console.log(event.target.userEmail.value);
-
-        if (selectDomain === "")
-            alert("Select your Domain");
-        else
-            console.log(selectDomain);
+        alert('Feature under Process!');
     }
 
     return <div id={styles.outer}>
         <form onSubmit={handleSubmit}>
-            <select id='orgTitle' form='orgSelectForm' onChange={updateDomain} value={selectDomain} required>
-                <option value="" disabled selected>Select Organization</option>
-                {populateOrgList()}
-            </select>
-            <br />
+
             <fieldset>
                 <legend>Domain Email</legend>
                 <input type="email" placeholder='xyz@domain.example' id='userEmail' required />
